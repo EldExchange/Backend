173be4092d30856081ab4dd707d0dbf3846b6f14
Felipe Pinheiro <flpinheiro@gmail.com>
2022-11-09 16:05:39 -0300

    feat: add entity framework
    feat: add dbContext
    
    feat: add db Configuration

---
+++ EldExchange.Infra/Config/InfraConfigExtensions.cs
@@ -0,0 +1,21 @@
+﻿using EldExchange.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldExchange.Infra.Config
+{
+    public static class InfraConfigExtensions
+    {
+        public static void AddInfraConfiguration(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var connectionString = configuration.GetConnectionString("EldExchange");
+            services.AddDbContext<EldDbContext>(context => context.UseSqlServer(connectionString));
+        }
+    }
+}
